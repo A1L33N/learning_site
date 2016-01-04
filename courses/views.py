dbc2bc13fb72af69179b54bc0680370266fcf802
Aileen Cacayorin <acacayorin@gmail.com>
2016-01-04 15:44:36 -0800

    create django admin user

--- courses/views.py
+++ courses/views.py
@@ -5,5 +5,5 @@ from .models import Course #imports from models.py
 # Create your views here.
 def course_list(request):
     courses = Course.objects.all() #selects all courses that exist
-    output = ', '.join(courses) #formats output of courses
+    output = ', '.join([str(course) for course in courses]) #formats output of courses - in this case, a for loop inside a list?
     return HttpResponse(output) #returns http response
